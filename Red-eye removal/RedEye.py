953989c75fb01c823b5aa25048a0791d95f41eb3
Bradley Bath <badfitz66@gmail.com>
2019-10-22 10:00:16 +0100

    Updated my code

--- Red-eye removal/RedEye.py
+++ Red-eye removal/RedEye.py
@@ -1,11 +1,11 @@
 import cv2,sys,numpy
 
-""" 
+"""
     Class with helper functions for removing red eyes from a photo 
     Original author: Satya Mallick(https://www.learnopencv.com/automatic-red-eye-remover-using-opencv-cpp-python/)
     
-    All credit goes to original author. I have only put the code code into a Python module
-    and also fixed an error where mean would stay as a float64. I fixed this by
+    All credit goes to original author. I have only put the code into a Python module
+    I also fixed an error where the variable mean would stay as a float64. I fixed this by
     changing:
         mean = [:, :, numpy.newaxis]
     to
@@ -13,8 +13,10 @@ import cv2,sys,numpy
 
     There is still an issue, however, where only one red eye is removed.
 """
+
+"""Helper function that fills holes in a mask"""
 def fillHoles(mask):
-    """ Helper function that fills holes in a mask"""
+    """Copy the mask"""
     maskFloodfill = mask.copy()
     h, w = maskFloodfill.shape[:2]
     maskTemp = numpy.zeros((h+2, w+2), numpy.uint8)
@@ -22,9 +24,8 @@ def fillHoles(mask):
     mask2 = cv2.bitwise_not(maskFloodfill)
     return mask2 | mask
 
-
+"""Remove red eyes from an image given it's directory, as-well as the directory to a valid HAAR cascade. Returns an image with the red eyes removed."""
 def RemoveRedEyes(Image_Path,Cascade_Classifier_Path):
-    """Remove red eyes from an image given it's directory, as-well as the directory to a valid HAAR cascade. Returns an image with the red eyes removed."""
 
     Image=cv2.imread(Image_Path,cv2.IMREAD_COLOR)
     HAAR_Cascade= cv2.CascadeClassifier(Cascade_Classifier_Path)
@@ -33,10 +34,9 @@ def RemoveRedEyes(Image_Path,Cascade_Classifier_Path):
     Image_Output=Image.copy()
 
     """Detect eyes in the image using the HAAR cascade"""
-
     eyes = HAAR_Cascade.detectMultiScale(Image,scaleFactor=1.3, minNeighbors=4, minSize=(100, 100))
     mask=int(0)
-    """Mask the eyes in the image"""
+    """Mask the eyes in the image(This makes the eyes white with the rest of the image black)"""
     for (x, y, w, h) in eyes:
         eye = Image[y:y+h, x:x+w]
         r = eye[:, :, 2]
@@ -49,7 +49,9 @@ def RemoveRedEyes(Image_Path,Cascade_Classifier_Path):
 
         mask = mask.astype(numpy.uint8)*255
     
+    """Fill any holes in the mask"""
     mask=fillHoles(mask)
+    """Dilate (enlarge) the mask"""
     mask=cv2.dilate(mask,None,anchor=(-1,-1),iterations=8,borderType=1,borderValue=1)
 
     """Calculate the mean channel by averaging the green and blue channels. Recall, bg = cv2.add(b, g)"""
